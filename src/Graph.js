c0afc47d6bc2b811dd004402c77588548995d3ed
João Reis <joao.reis@OPO-MBP-0110.local>
2019-07-19 18:04:03 +0100

    promise

--- src/Graph.js
+++ src/Graph.js
@@ -5,17 +5,20 @@ import Confetti from 'react-confetti';
 import SlidingPane from 'react-sliding-pane';
 import ReactJson from "react-json-view"
 import 'react-sliding-pane/dist/react-sliding-pane.css';
-
-
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+import neo4j from 'neo4j-driver'
 
 class Graph extends PureComponent {
+  
   constructor() {
     super();
     this.state = {
       isLoading: true,
       currentInteractionID: "5d31bc15d2562700045be1f5",
       graphQuery: this.getQueryByInteractionID("5d31bc15d2562700045be1f5"),
-      paneOpen: false
+      paneOpen: false,
+      options: this.getList()
     }
 
     this.search = this.search.bind(this);
@@ -30,11 +33,11 @@ class Graph extends PureComponent {
       server_password: "i-089a33b376c7147cb",
       labels: {
         "Events": {
-                      "caption": "type",
-                  },
-                  "Models": {
-                                "caption": "type"
-                            },
+          "caption": "type",
+        },
+        "Models": {
+          "caption": "type"
+        },
       },
       nodes: {
         color: {
@@ -42,10 +45,10 @@ class Graph extends PureComponent {
         }
       },
       relationships: {
-          Uses: {
-              thickness: "weight",
-              caption: false
-          }
+        Uses: {
+          thickness: "weight",
+          caption: false
+        }
       },
       hierarchical: true,
       initial_cypher: this.state.graphQuery,
@@ -55,17 +58,17 @@ class Graph extends PureComponent {
 
     this.viz.render();
     this.viz.registerOnEvent("completed", () => {
-      this.setState({ isLoading: false });
-      this.viz._network.on('selectNode', (evt) =>  {
+      this.setState({ isLoading: false });
+      this.viz._network.on('selectNode', (evt) => {
         if (evt.nodes.length > 0) {
           console.log(this.viz._nodes[evt.nodes[0]]);
 
-        const parsedInfo = JSON.parse(
-          this.viz._nodes[evt.nodes[0]].title.split("<strong>payload:</strong>")[1].split("<strong>timestamp:</strong>")[0].replace("<br>", "")
-        );
-        this.setState({ info: parsedInfo, paneOpen: true });
+          const parsedInfo = JSON.parse(
+            this.viz._nodes[evt.nodes[0]].title.split("<strong>payload:</strong>")[1].split("<strong>timestamp:</strong>")[0].replace("<br>", "")
+          );
+          this.setState({ info: parsedInfo, paneOpen: true });
         }
-});
+      });
 
     });
 
@@ -75,34 +78,62 @@ class Graph extends PureComponent {
     return `MATCH (m {interaction_id: '${interactionID}'})-[r:Uses*]-(c) RETURN c, m, r ORDER by m.timestamp`
   }
 
+  getQueryAllInteractionList() {
+    return `MATCH (m:Models) WITH DISTINCT m.interaction_id AS ids RETURN ids`
+  }
+
   handleInputUpdate(evt) {
-    this.setState({ currentInteractionID: evt.currentTarget.value });
+    this.setState({ currentInteractionID: evt.currentTarget.value });
   }
 
   search() {
-    this.viz.renderWithCypher(this.getQueryByInteractionID(this.state. currentInteractionID));
-    this.setState({ isLoading: true });
+    this.viz.renderWithCypher(this.getQueryByInteractionID(this.state.currentInteractionID));
+    this.setState({ isLoading: true });
   }
 
-  render (){
+
+
+  getList() {
+    var Listaaa =[]
+    var driver = neo4j.driver(
+      'bolt://54.226.181.219:7687',
+      neo4j.auth.basic('neo4j', 'i-089a33b376c7147cb')
+    )
+    var session = driver.session()
+    return session
+      .run(`MATCH (m:Models) WITH DISTINCT m.interaction_id AS ids RETURN ids`)
+      .then(function(result) {
+        var Lista= result.records.map(x => x.get('ids'))
+        session.close()
+        return Lista
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
+
+  render() {
+    var x= this.getList()
+    console.log(x)
     return (
       <Fragment>
+        <Dropdown options={this.state.options} onChange={this._onSelect} placeholder="Select an option" />
         {this.state.isLoading ? (
           <Confetti
-             width={1000}
-             height={1000}
-           />
+            width={1000}
+            height={1000}
+          />
         ) : null}
         <input value={this.state.currentInteractionID} onChange={this.handleInputUpdate} />
         <button onClick={this.search}>Search</button>
         <SlidingPane
-              isOpen={this.state.paneOpen}
-              from='right'
-              width='400px'
-              onRequestClose={ () => this.setState({ paneOpen: false }) }>
-              <ReactJson src={this.state.info} theme="bright:inverted" />
-          </SlidingPane>
-        <div id="viz" style={{width: "auto", height: "1000px"}}/>
+          isOpen={this.state.paneOpen}
+          from='right'
+          width='400px'
+          onRequestClose={() => this.setState({ paneOpen: false })}>
+          <ReactJson src={this.state.info} theme="bright:inverted" />
+        </SlidingPane>
+        <div id="viz" style={{ width: "auto", height: "1000px" }} />
       </Fragment>
     );
   }
